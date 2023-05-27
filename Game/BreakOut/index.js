f2156b5f7e7ee3d41b954de8ebb6ed21d6b451d7
MazidulHasan <md.mazidulhasan1@gmail.com>
2023-05-27 16:52:10 +0600

    Working on BreakOut

---
+++ Game/BreakOut/index.js
@@ -0,0 +1,7 @@
+const grid = document.querySelector('.grid')
+
+const block = document.createElement('div')
+block.classList.add('block')
+block.style.left = '100px'
+block.style.right = '50px'
+grid.appendChild(block)
