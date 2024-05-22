f9e6761227492aaf7952579b335b9be7cd3b2eda
Mazidul <md.mazidulhasan1@gmail.com>
2024-05-22 23:57:01 +0600

    Objects

---
+++ Basic/LearnWithSumit/Object/objectTest01.js
@@ -0,0 +1,12 @@
+let car = {
+    name:"Honda",
+    model: 200,
+    weight:"900KG",
+    color:"Red",
+    start: function(){
+        console.log('Car start');
+    },
+    drive: function() {
+        console.log('Car is driving');
+    }
+}
